d9471cc1cc64510803288791e39b08a50ea14540
tdtrung5893 <tranducha23081967@gmail.com>
2018-07-25 11:59:38 +0700

    ref Taask#6 show progress lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
   has_many :words, dependent: :destroy
-  has_many :user_lessons
+  has_many :user_lessons, dependent: :destroy
   belongs_to :category
 
   scope :load_lesson, -> {select :id, :name}
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ApplicationRecord
   has_many :answers, dependent: :destroy
-  has_many :user_words
+  has_many :user_words, dependent: :destroy
   belongs_to :lesson
 
   accepts_nested_attributes_for :user_words
